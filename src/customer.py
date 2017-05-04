fa25a75a26236b90c8e6a1428a481e7aa67cd885
Taylor Perkinse <perkinstaylor.luthier@gmail.com>
2017-05-04 13:27:37 -0500

    Created initial unit tests for TestCustomer

--- src/customer.py
+++ src/customer.py
@@ -1,1 +1,49 @@
-.
+# .
+
+class Customer(object):
+    """docstring for Customer"""
+
+    def __init__(self, name, street_address, city, state, postal_code, phone_number):
+        self.__name = name
+        self.__street_address = street_address
+        self.__city = city
+        self.__state = state
+        self.__postal_code = postal_code
+        self.__phone_number = phone_number
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def street_address(self):
+        return self.__street_address
+
+    @property
+    def city(self):
+        return self.__city
+
+    @property
+    def state(self):
+        return self.__state
+
+    @property
+    def postal_code(self):
+        return self.__postal_code
+
+    @property
+    def phone_number(self):
+        return self.__phone_number
+
+    @property
+    def id(self):
+        return self.__id  
+
+    def save(self):
+        self.__id = 1
+
+
+# @street_address.setter
+# def street_address(self, street_address):
+#   self.__street_address = street_address
+
--- test/TestCustomer.py
+++ test/TestCustomer.py
@@ -1,1 +1,40 @@
-.
+import unittest
+
+import sys; sys.path.append('../')
+from src.customer import Customer
+
+class TestCustomer(unittest.TestCase):
+
+  @classmethod
+  def setUpClass(self):
+    self.taylor = Customer(name="Taylor Unicorn", street_address="Nashville", city="1st Avenue", state="Nick", postal_code="12345-67894", phone_number="615-999-9999")
+    self.taylor.save()
+
+  def test_customer_has_name(self):
+    self.assertEqual(self.taylor.name, 'Taylor Unicorn')
+
+  def test_customer_has_street_address(self):
+    self.assertEqual(self.taylor.street_address, 'Nashville')
+
+  def test_customer_has_city(self):
+    self.assertEqual(self.taylor.city, '1st Avenue')
+
+  def test_customer_has_state(self):
+    self.assertEqual(self.taylor.state, 'Nick')
+
+  def test_customer_has_postal_code(self):
+    self.assertEqual(self.taylor.postal_code, '12345-67894')
+
+  def test_customer_has_phone_number(self):
+    self.assertEqual(self.taylor.phone_number, '615-999-9999')
+
+  def test_customer_exists_in_database(self):
+    self.assertIsNotNone(self.taylor.id)
+
+
+
+
+
+
+
+
